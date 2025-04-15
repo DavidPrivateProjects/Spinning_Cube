f3110cd6e8f9aced90b7c5a61266e962a20c965f
DavidPrivateProjects <david.petrovic@akane.waseda.jp>
2025-04-15 21:43:14 +0200

    feat: implements cube variables

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import math
 from math import cos as cos
 from math import sin as sin
 
+cube_width = 10
+width, height = 160, 44
+zbuffer = 160 * 44
+buffer = 160 * 44
+background_ascii_code = ' '
+inc_speed = 1
+
 def get_x(i, j, k):
     return (  j * sin(a) * sin(b) * cos(c) 
             - k * cos(a) * sin(b) * cos(c)
@@ -20,3 +27,13 @@ def get_z(i, j, k):
     return (  k * cos(a) * cos(b) 
             - j * sin(a) * cos(b)
             + i * sin(b))
+
+
+def main():
+    while True:
+        background = [background_ascii_code * width * height]
+        for x in range(-cube_width, cube_width, inc_speed):
+            for y in range(-cube_width, cube_width, inc_speed):
+                for z in range(-cube_width, cube_width, inc_speed):
+
+main()
